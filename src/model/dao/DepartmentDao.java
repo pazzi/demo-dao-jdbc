c10bcc9edf6363272eee13ded0c2066b1114b1fc
pazzi <carlos.pazzianotto@gmail.com>
2023-02-04 00:26:58 -0300

    Acerto em nome department

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import model.entities.Department;
 
-public interface DepatmentDao {
+public interface DepartmentDao {
 	
 	void insert(Department obj);
 	void update(Department obj);
