9c168a9078fcdc13dd8d6066af4b46d572f32462
pazzi <carlos.pazzianotto@gmail.com>
2023-02-05 15:39:55 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,14 @@ public class Program {
 			sellerDao.insert(newSeller);
 			System.out.println("Inserted new id:"+ newSeller.getId());
 			
+			
+			System.out.println("=== TEST 5: seller update ===");
+			seller = sellerDao.findById(1);
+			seller.setName("Martha Waine");
+			sellerDao.update(seller);
+			System.out.println("Update complete!");
+
+			
 	}
 
 }
